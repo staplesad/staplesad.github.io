2bc16346ef001137bab1dfc08692104152334b6c
staplesad <sadhbhsdoyle@gmail.com>
2023-11-18 22:32:17 +0100

    make it look a bit better

--- load_json_table.js
+++ load_json_table.js
@@ -2,6 +2,10 @@ function load_json(file){
   return fetch(file).then((response) => response.json())
 }
 
+function get_pdb_url(name){
+  base_url = "https://pokemondb.net/pokedex/"
+  return base_url + name
+}
 function tableCreate(data) {
   const div = document.getElementById("ev_table"),
   tbl = document.createElement('table');
@@ -16,16 +20,16 @@ function tableCreate(data) {
     const tr = tbl.insertRow();
     for (let j = 0; j < 2; j++) {
       const td = tr.insertCell();
-      let value = ""
       if (j===0) {
-        console.log("if")
-        value = n
+        link = document.createElement('a');
+        link.href = get_pdb_url(key);
+        textnode = document.createTextNode(`${n}\t\t`);
+        link.appendChild(textnode);
+        td.appendChild(link);
       }
       else {
-        console.log("else")
-        value = ev
+        td.appendChild(document.createTextNode(`${ev}\t\t`));
       }
-      td.appendChild(document.createTextNode(`${value}\t\t`));
     }
   }
   div.appendChild(tbl);
